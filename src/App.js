c03905a1ab299af4af95d767bdfc4cf900cc272c
Jeremy Siaw <jx.siaw@gmail.com>
2019-03-14 22:14:11 +1100

    Initialie Repository

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Map from './components/map/map';
+
+class App extends Component {
+  render() {
+    return (
+      <Map />
+    );
+  }
+}
+
+export default App;

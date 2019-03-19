c0b2a602976b87392a993a00634430837bd35a37
Jeremy Siaw <jx.siaw@gmail.com>
2019-03-20 09:49:49 +1100

    Using a different tile map

--- src/views/Map/Map.js
+++ src/views/Map/Map.js
@@ -21,7 +21,7 @@ export default class Map extends Component {
             <LeafletMap center={position} zoom={this.state.zoom}>
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url='https://maps.tilehosting.com/styles/basic/{z}/{x}/{y}.png?key=ljh1rAY6bM1lt0B7oi6Y'
                 />
                 <Marker position={[this.state.lat, this.state.lng]} />
             </LeafletMap>

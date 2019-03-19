2be679caad5e08c2ac1e7a8ba1ed2b84f9490d6a
XephiZ <45624944+JX-Siaw@users.noreply.github.com>
2019-03-19 19:18:44 +1100

    Add build directory :fire:

---
+++ build/precache-manifest.34fb6231d874004928aca63c1d6f687c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df46d89ecec00a160b75",
+    "url": "/static/css/main.6dcb9fcb.chunk.css"
+  },
+  {
+    "revision": "df46d89ecec00a160b75",
+    "url": "/static/js/main.df46d89e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7fa00853dc6b1ed978f2",
+    "url": "/static/js/2.7fa00853.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "1cb1004acad95a65250e4ef7c337a594",
+    "url": "/index.html"
+  }
+];
